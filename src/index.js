a77bc466a767b8a0ea7a3752fac72ba007add103
ecniv <167593601+ecniv18@users.noreply.github>
2024-10-10 21:52:53 +0800

    Completely render UI dynamically instead of hard coded on html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './css/main.css';
+import { appendElement } from './helper/append-element';
+import Header from './UI/header/Header';
+
+const body = document.querySelector('.wrapper');
+
+appendElement(body, Header());
